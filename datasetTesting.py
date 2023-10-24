06c1041754ca7bbee88d60e292160e199eabccdd
monirulHaque <monirul.haque@g.bracu.ac.bd>
2023-10-24 14:32:37 +0600

    Rearranged codes and added comments

--- datasetTesting.py
+++ datasetTesting.py
@@ -1,23 +1,26 @@
 import sys
 import subprocess
 import os
+import ir_datasets
 
 # implement pip as a subprocess:
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
 'ir_datasets'])
 
+# clear the terminal after pip installation
 clear = lambda: os.system('clear')
 if os.name == 'nt':
     clear = lambda: os.system('cls')
-
 clear()
 
-import ir_datasets
+# loading dataset for queries
 dataset = ir_datasets.load("wapo/v2/trec-news-2019")
 
+# check callable methods of a class
 def getObjectMethods(object):
     return [method_name for method_name in dir(object) if callable(getattr(object, method_name))]
 
+# testing queries_iter()
 for query in dataset.queries_iter():
     print(query)
     print(type(query))
@@ -27,6 +30,7 @@ for query in dataset.queries_iter():
     print(query.url)
     break
 
+# testing qrels_iter()
 for qrels in dataset.qrels_iter():
     print(qrels)
     print(qrels.query_id)
@@ -35,6 +39,7 @@ for qrels in dataset.qrels_iter():
     print(qrels.iteration)
     break
 
+# learning about the dataset
 print("Total Queries:", dataset.queries_count())
 print("Total Docs:", dataset.docs_count())
 print("Total qrels:", dataset.qrels_count())
@@ -42,7 +47,7 @@ print("Has Doc Pairs?:", dataset.has_docpairs())
 print("Has Doc Scores?:", dataset.has_scoreddocs())
 print("Doc class", dataset.docs_cls())
 
-
+# loading dataset for documents
 dataset = ir_datasets.load("wapo/v2")
 for doc in dataset.docs_iter():
     print(doc)
